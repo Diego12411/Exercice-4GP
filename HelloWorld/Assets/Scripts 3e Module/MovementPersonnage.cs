a66b7fb136584971c1f9899612327cc2472db454
Reinoso Breault <6304136@bdeb.qc.ca>
2026-02-04 11:18:07 -0500

    Rotation-Module3 fini

--- HelloWorld/Assets/Scripts 3e Module/MovementPersonnage.cs
+++ HelloWorld/Assets/Scripts 3e Module/MovementPersonnage.cs
@@ -1,32 +1,54 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MovementPersonnage : MonoBehaviour
 {
-    private IEnumerator DeplacerPersonnage(Vector3 direction);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Vector3 direction;
+    private Vector3 objectif;
+    private Quaternion rotationFinale;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
+
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit)) //question, cest quoi et comment ca marche le ray, cest quoi RayCast, cest quoi
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Vector3 direction = (transform.position - hit.point).normalized;
-                while ((transform.position - objectif).magnitude > 0.5f){
-                    transform.position += Time.deltaTime * direction;
-                    yield return null;
-                }
+                Vector3 direction = (hit.point - transform.position).normalized;
+                objectif = hit.point;
+                rotationFinale = Quaternion.LookRotation(direction);
+                Coroutine coDeplace = StartCoroutine(DeplacerPersonnage(direction));
+                Coroutine coRotation = StartCoroutine(RotaterPersonnage(rotationFinale));
             }
         }
 
     }
-}
+        IEnumerator DeplacerPersonnage(Vector3 direction) {
+        while ((transform.position - objectif).magnitude > 0.5f)
+        {
+            transform.position += Time.deltaTime * direction;
+            yield return null;
+        }
+        StopCoroutine(DeplacerPersonnage(direction));
+    }
+
+        IEnumerator RotaterPersonnage(Quaternion rotationFinale)
+    {
+        while (transform.rotation != rotationFinale)
+        {
+            var rotate = 50 * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationFinale, rotate);
+            yield return null;
+        }
+        StopCoroutine(RotaterPersonnage(rotationFinale));
+    }
+ }
